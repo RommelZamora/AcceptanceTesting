0b43b2f026fdb0fb34df180aefe3caf9a784f283
RommelZamora <95447422+RommelZamora@users.noreply.github.com>
2024-08-08 16:42:46 -0500

    Add files via upload

---
+++ todo_list.py
@@ -0,0 +1,9 @@
+#TO DO LIST APPLICATION
+
+"""Description: The To-Do List Manager is a command-line application that allows users to
+manage their tasks by adding, listing, and marking them as complete.
+A task can have the attributes you think will fit the most, but minimum 4.
+"""
+
+to_do_list = []
+
